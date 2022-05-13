aa83680567972aec1b1aac60ba268daac5264708
Your Name <you@example.com>
2022-05-13 17:35:23 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,14 +44,14 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-  int i,j,p,num3[10] = {0};
-  for(i=1 ; i<10 ; i++)
+  int i,j,p,num3[9] = {0};
+  for(i=0 ; i<9 ; i++)
   {
-    int num1[10] = {0};
-    int num2[10] = {0};
+    int num1[9] = {0};
+    int num2[9] = {0};
 
     //recorrer en diagonal (i,i) y revisar filas y columnas a partir de ese punto
-    for(j=1;j<10;j++)
+    for(j=0;j<9;j++)
     {
       printf("Verificando posicion [%i,%i] = %i\n",i,j,n->sudo[i][j]);
       //filas desde i,i
